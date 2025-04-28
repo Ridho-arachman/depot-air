a589a4fc5d6e5f659581661c61ada7718137f221
Ridho-arachman <ridho.arachman22@gmail.com>
2025-04-28 10:29:03 +0700

    chore(mobile): menambahkan library axios

--- mobile/app/(tabs)/pesanan.tsx
+++ mobile/app/(tabs)/pesanan.tsx
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { useStore } from "@/store/useStore"; // Sesuaikan dengan path store Anda
+import { Button, Input, Text, View } from "tamagui";
 
-const pesanan = () => {
+const Pesanan = () => {
+  const store = useStore((state) => state.bears);
+  const tambah = useStore((state) => state.increasePopulation);
+  const kurang = useStore((state) => state.removeAllBears);
+  const updateBears = useStore((state) => state.updateBears);
+
   return (
-    <View>
-      <Text>pesanan</Text>
+    <View flex={1} justifyContent="center" alignItems="center">
+      <Text color={"red"}>{store}</Text>
+      <Button onPress={tambah}>TAMBAH</Button>
+      <Button onPress={kurang}>KURANG</Button>
+      <Input
+        value={store.toString()}
+        onChangeText={(e: string) => {
+          const num = parseInt(e);
+          updateBears(num);
+        }}
+        placeholder="Masukkan angka"
+      />
     </View>
   );
 };
 
-export default pesanan;
+export default Pesanan;
 
 const styles = StyleSheet.create({});
